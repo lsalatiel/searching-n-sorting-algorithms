3324aa181c5b61adb2802e56832c2679471f03cb
lsalatiel <lucassalatiel1004@gmail.com>
2023-08-29 18:48:23 -0300

    selection sort

---
+++ sort_algorithms/selection_sort.c
@@ -0,0 +1,15 @@
+#include "item.h"
+#include <stdlib.h>
+
+
+void sort(Item *a, int lo, int hi) {
+    Item min;
+    for(int i = lo; i < hi; i++) {
+        min = i;
+        for(int j = i + 1; j < hi; j++) {
+            if(less(a[j], a[min]))
+                min = j;
+        }
+        exch(a[i], a[min]);
+    }
+}
