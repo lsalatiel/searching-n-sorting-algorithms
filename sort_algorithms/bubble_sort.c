5cfea2100e64abbaaedbc8d40251d43bfefc9393
lsalatiel <lucassalatiel1004@gmail.com>
2023-09-04 19:48:12 -0300

    bubble sort

--- sort_algorithms/bubble_sort.c
+++ sort_algorithms/bubble_sort.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 void sort(Item *a, int lo, int hi) {
-    for(int i = lo; i < hi; i++) {
+    for(int i = lo; i <= hi; i++) {
         int swapped = 0;
-        for(int j = lo; j < hi - i - 1; j++) {
+        for(int j = lo; j < hi - i; j++) {
             if(less(a[j + 1], a[j])) {
                 exch(a[j + 1], a[j]);
                 swapped = 1;
