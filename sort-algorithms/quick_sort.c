0568d9f4f18edaece765bb601939fb8ae16c5db5
lsalatiel <lucassalatiel1004@gmail.com>
2023-09-30 01:18:46 -0300

    add cutoff for insertion sort

--- sort-algorithms/quick_sort.c
+++ sort-algorithms/quick_sort.c
@@ -31,11 +31,34 @@ void shuffle(Item *a, int N) {
     }
 }
 
+#ifdef CUTOFF
+void insertion_sort(Item *a, int lo, int hi) {
+    for (int i = hi; i > lo; i--)
+        compexch(a[i-1], a[i]);
+
+    for (int i = lo+2; i <= hi; i++) {
+        int j = i;
+        Item v = a[i];
+
+        while (less(v, a[j-1])) {
+            a[j] = a[j-1];
+            j--;
+        }
+        a[j] = v;
+    }
+}
+#endif
+
 void quick_sort(Item *a, int lo, int hi) {
-    if(hi <= lo) {
+    #ifdef CUTOFF
+    if(hi <= lo + CUTOFF - 1) {
+        insertion_sort(a, lo, hi);
         return;
     } 
-    
+    #else
+    if(hi <= lo)
+        return;
+    #endif
     int j = partition(a, lo, hi);
     quick_sort(a, lo, j-1);
     quick_sort(a, j+1, hi);
